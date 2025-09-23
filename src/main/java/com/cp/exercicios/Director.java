3d6f725326811f9d486bc9d9d9b91d4bfe8b9436
labsfiap <rickinho.celso08@gmail.com>
2025-09-23 11:26:28 -0300

    tudo atuizado

---
+++ src/main/java/com/cp/exercicios/Director.java
@@ -0,0 +1,24 @@
+package com.cp.exercicios;
+
+public class Director extends Manager{
+    private double porcentagemParticipacaoLucros;
+
+    public Director(String name, String cpf, Double wage, Double bonus, double porcentagemParticipacaoLucros) {
+        super(name, cpf, wage, bonus);
+        this.porcentagemParticipacaoLucros = porcentagemParticipacaoLucros;
+
+    }
+
+    public double getPorcentagemParticipacaoLucros() {
+        return porcentagemParticipacaoLucros;
+    }
+
+    public void setPorcentagemParticipacaoLucros(double porcentagemParticipacaoLucros) {
+        this.porcentagemParticipacaoLucros = porcentagemParticipacaoLucros;
+    }
+
+    @Override
+    public Double getWage(){
+        return super.getWage() * porcentagemParticipacaoLucros;
+    }
+}
--- src/main/java/com/cp/exercicios/Employee.java
+++ src/main/java/com/cp/exercicios/Employee.java
@@ -3,7 +3,7 @@ package com.cp.exercicios;
 public class Employee {
     String name;
     String cpf;
-    Double wage;
+    private Double wage;
 
     public Employee(String name, String cpf, Double wage) {
         this.name = name;
@@ -18,4 +18,17 @@ public class Employee {
     public Double getWage() {
         return wage;
     }
+
+    public void aumentaWage(){
+        this.wage *= 1.10;
+    }
+
+    public void aumentaWage( double porcent){
+        this.wage *= (1 + porcent/ 100.0);
+    }
+
+    public void aumentaWage( double porcent, double bonus){
+        this.wage *= this.wage + (1 + porcent/100.0) + bonus;
+    }
 }
+
